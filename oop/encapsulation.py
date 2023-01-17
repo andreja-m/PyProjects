eb07d8e9aadae420a0c2eb1ec4fae75a0239ca10
Andreja Milosavljevic <77466950+doomguy-369@users.noreply.github.com>
2023-01-17 14:53:35 +0100

    Add files via upload

---
+++ oop/encapsulation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+# Python program to
+# demonstrate private members
+
+# Creating a Base class
+
+class Base:
+    def __init__(self):
+        self.a="GeeksforGeeks"
+        self.__c="GeeksforGeeks"
+
+class Derived(Base):
+    def __init__(self):
+        
+        # Calling constructor of
+        # Base class
+        Base.__init__(self)
+        print("Calling private member of base class: ")
+        print(self.__c)
+
+# Driver code
+obj1=Base()
+print(obj1.a)
+
