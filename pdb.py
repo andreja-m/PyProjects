d08dce7e3542fb8d06ee02d5aa3ba2dec53675aa
Andreja Milosavljevic <77466950+doomguy-369@users.noreply.github.com>
2023-02-01 15:44:13 +0100

    Add files via upload

---
+++ pdb.py
@@ -0,0 +1,7 @@
+# pdb.py can also be invoked as a script to debug other scripts. For example:
+
+python3 -m pdb myscript.py
+
+# The typical usage to break into the debugger is to insert:
+
+import pdb; pdb.set_trace()
